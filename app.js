5df1fa9e43050c3b1b0c7141d3b647bbc3f6f3b6
saim083 <saimahmed865@gmail.com>
2020-04-25 04:49:49 +0000

    installing packages

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ router.post('/post/json', function(req, res) {
     xmlFileToJs('KFCRestaurant.xml', function(err, result) {
       if (err) throw (err);
       //This is where you pass on information from the form inside index.html in a form of JSON and navigate through our JSON (XML) file to create a new entree object
-      result.cafemenu.section[obj.sec_n].entree.push({'item': obj.item, 'price': obj.price}); //If your XML elements are differet, this is where you have to change to your own element names
+      result.KFCmenu.section[obj.sec_n].entree.push({'item': obj.item, 'price': obj.price}); //If your XML elements are differet, this is where you have to change to your own element names
       //Converting back to our original XML file from JSON
       jsToXmlFile('KFCRestaurant.xml', result, function(err) {
         if (err) console.log(err);
@@ -72,6 +72,7 @@ router.post('/post/json', function(req, res) {
 
   // Call appendJSON function and pass in body of the current POST request
   appendJSON(req.body);
+
 
   // Re-direct the browser back to the page, where the POST request came from
   res.redirect('back');
@@ -87,7 +88,7 @@ router.post('/post/delete', function(req, res) {
     xmlFileToJs('KFCRestaurant.xml', function(err, result) {
       if (err) throw (err);
       //This is where we delete the object based on the position of the section and position of the entree, as being passed on from index.html
-      delete result.cafemenu.section[obj.section].entree[obj.entree];
+      delete result.KFCmenu.section[obj.section].entree[obj.entree];
       //This is where we convert from JSON and write back our XML file
       jsToXmlFile('KFCRestaurant.xml', result, function(err) {
         if (err) console.log(err);
